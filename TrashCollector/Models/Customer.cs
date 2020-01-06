025fcfb7fc2b39ea2534b7794c1606ff5132f3e3
Elias Holter <eliasholter1@gmail.com>
2020-01-06 15:27:59 -0600

    Initial Commit, established local DB and Customer and Employee models

---
+++ TrashCollector/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        public Customer()
+        {
+
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PickUpDay { get; set; }
+        public string ExtraPickUpDay { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public double Balance { get; set; }
+        public string SuspendStart { get; set; }
+        public string SuspendEnd { get; set; }
+        public bool PickUpConfirmation { get; set; }
+
+    }
+}
---
+++ TrashCollector/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
