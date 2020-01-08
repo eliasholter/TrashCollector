e08aaf312f4b5ac699d4b3687fe479cc62ef6aee
Elias Holter <eliasholter1@gmail.com>
2020-01-08 10:45:23 -0600

    Added ability to create an employee user, and when it's created it will display a list of the customers that need a pick up on the current day and are within the zip code of the employee

--- TrashCollector/Controllers/EmployeeController.cs
+++ TrashCollector/Controllers/EmployeeController.cs
@@ -20,7 +20,11 @@ namespace TrashCollector.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            string userId = User.Identity.GetUserId();
+            string currentDay = DateTime.Today.DayOfWeek.ToString();
+            var employee = context.Employees.Where(e => e.ApplicationId == userId).FirstOrDefault();
+            var customersOnRoute = context.Customer.Where(c => (c.ZipCode == employee.ZipCode) && (currentDay == c.PickUpDay)).ToList();
+            return View(customersOnRoute);
         }
 
         // GET: Employee/Details/5
